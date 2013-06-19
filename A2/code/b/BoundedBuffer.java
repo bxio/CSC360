5af1c742ac431ddea7740b2cab30c724da3b66d9
Bill Xiong <billxiong99@gmail.com>
2013-06-19 16:39:53 -0700

    Added Part B

--- A2/code/b/BoundedBuffer.java
+++ A2/code/b/BoundedBuffer.java
@@ -6,41 +6,64 @@ import uvic.posix.*;
 public class BoundedBuffer implements SBuffer{
 
 	private Mutex m;
-	private Condition nonfull, nonempty;
+	private Condition nonfull, nonempty, waitingReaders;
 	private int buffer[];
 	private int size;
-	private int in, out, count;
-	private int readers[];
+	private int in, out, count, countReaders, rd, wr;
+	private boolean writerActive;
+	
 
 	public BoundedBuffer( int n ) {
 		m = new Mutex();
 		nonfull = new Condition();
 		nonempty = new Condition();
+		waitingReaders = new Condition();
 		buffer = new int[n];
 		size = n;
 		count = 0;
+		countReaders = 0;
 		in = 0;
 		out = 0;
+		rd = 0;
+		wr = 0;
+		writerActive = false;
 	}
 
 	/** Start reading an item at location i wait if buffer is empty. */
     public void StartRead( int i ) {
-		// DELETEME: Fill in
+		int j = i%size;
+		m.Lock();
+		
+		if(j == out){ //if the thread is trying to read the same location as "out", continue
+			++countReaders;
+		}
+		else { //if the thread is trying to read ahead of the "out" location
+			waitingReaders.Wait(m); //wait for other readers to finish
+			++countReaders;
+		}		
+		m.UnLock();
 	}
 
     /** Done reading. */
     public void EndRead() {
-		// DELETEME: Fill in
+		if(countReaders == 0){
+
+			waitingReaders.Broadcast();
+			
+		}	
 	}
 
     /** Start writing an item wait if buffer is full. */
     public void StartWrite() {
-		// DELETEME: Fill in
+		
+		
+		
+			
 	}
 
     /** Done writing. */
     public void EndWrite() {
-		// DELETEME: Fill in
+		++wr;
 	}
 	
 
@@ -62,8 +85,11 @@ public class BoundedBuffer implements SBuffer{
 		m.Lock();
 		while (count == 0) nonempty.Wait(m);
 		v = buffer[out];
-		out = (out + 1) % size;
-		--count;
+		--countReaders;
+		if(countReaders == 0) {
+			out = (out + 1) % size;
+			--count;
+		}
 		nonfull.Signal();
 		m.UnLock();
 
