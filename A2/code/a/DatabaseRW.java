23f1af6abbd92f46481ebe9ac50da78d29ac6933
Bill Xiong <billxiong99@gmail.com>
2013-06-18 22:33:17 -0700

    Part A readers still clumping around writer 17.

--- A2/code/a/DatabaseRW.java
+++ A2/code/a/DatabaseRW.java
@@ -7,99 +7,84 @@ import uvic.posix.*;
 public class DatabaseRW implements Database {
 
 	private Mutex m;
-	private Condition readQ;
+    private Condition readQ;
 	private Condition writeQ;
-	private int waitingReaders;
+    private int waitingReaders;
 	private int activeReaders;
+	private int activeWriter;
 	private int waitingWriters;
-	private int activeWriters;
-	private int dbState;
+    private int dbState;
 
-	private int val;
+    private int val;
 
-	private static final int NOTUSED = 0;
-	private static final int READING = 1;
-	private static final int WRITING = 2;
+    private static final int NOTUSED = 0;
+    private static final int READING = 1;
+    private static final int WRITING = 2;
 
-	public DatabaseRW() {
+    public DatabaseRW() {
 		m = new Mutex();
 		readQ = new Condition();
 		writeQ = new Condition();
-		activeReaders = waitingReaders = activeWriters = waitingWriters = 0;
+		activeReaders = waitingReaders = activeWriter = waitingWriters = 0;
 		dbState = NOTUSED;
 		val = 0;
-	}
+    }
 
 
-	public void StartRead() {
-		m.Lock();
-		while (dbState == WRITING){
-			++waitingReaders; //reader is now blocked on writing state
+    public void StartRead() {
+	    	m.Lock();
+		if (dbState == WRITING){
+			++waitingReaders;
 			readQ.Wait(m);
-			--waitingReaders;  //switches the state of the reader
+			--waitingReaders;
 		}
-		++activeReaders; //switches the state of the reader
-		if (activeReaders == 1){
-			dbState = READING; // set the state to reading
-		}
+		++activeReaders;
+		if (activeReaders == 1) dbState = READING;
 		m.UnLock();
-	}
+    }
 
 
-	public void EndRead() {
+    public void EndRead() {
 		m.Lock();
-		--activeReaders;//decrement count of active readers
+		--activeReaders;
 
-		if (activeReaders == 0 && waitingWriters > 0) { // I'm the last to read And there is someone waiting to write
-			dbState = NOTUSED; // pass the state to writing
+		if (activeReaders == 0 && waitingWriters > 0) { // I'm the last to read
+			dbState = WRITING;
 			writeQ.Signal();
 		}
-		else if(activeReaders == 0)
-		{
-			dbState = NOTUSED;
-		}
-
 		m.UnLock();
-	}
+    }
 
 
-public void StartWrite() {
-	m.Lock();
-	while (dbState != NOTUSED){
-		++waitingWriters;
-		writeQ.Wait(m);
-		--waitingWriters;
-	}
-	++activeWriters;
-	dbState = WRITING;
-	m.UnLock();
-}
+    public void StartWrite() {
+		m.Lock();
+		if (activeWriter == 1 && dbState != NOTUSED){
+			++waitingWriters;
+			writeQ.Wait(m);
+			--waitingWriters;
+		}
+		activeWriter = 1;
+		dbState = WRITING;
+		m.UnLock();
+    }
 
 
-public void EndWrite() {
-	m.Lock();
-	--activeWriters;
-	if(waitingWriters > 0) { //There's someone waiting to write.
-		//don't change the state
-		writeQ.Signal();
-	}
-	else  if(waitingReaders > 0){//I'm the last writier and someone is waiting to read
-		dbState = READING;
-		readQ.Broadcast();
-	}
-	else{
+    public void EndWrite() {
+		m.Lock();
+		activeWriter = 0;
 		dbState = NOTUSED;
-	}
-	m.UnLock();
-}
+		if(waitingWriters > 0) writeQ.Signal();
+		else readQ.Broadcast();
+		m.UnLock();
+    }
 
 
-public void Put( int i ) {
-	val = i;
-}
+    public void Put( int i ) {
+		val = i;
+    }
 
 
-public int Get() {
-	return val;
-}
+    public int Get() {
+		return val;
+    }
 }
