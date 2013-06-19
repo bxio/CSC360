58238adc66a9c11018b6f59209c421714a2b19e3
Bill Xiong <billxiong99@gmail.com>
2013-06-18 20:11:53 -0700

    Fixed indent in DatabaseRW.java

--- A2/code/a/DatabaseRW.java
+++ A2/code/a/DatabaseRW.java
@@ -2,36 +2,36 @@ import uvic.posix.*;
 
 /**
  This class defines a shared database for mulitple readers and
- writers to be implemented by you. 
+ writers to be implemented by you.
 */
 public class DatabaseRW implements Database {
 
 	private Mutex m;
-    private Condition readQ;
+	private Condition readQ;
 	private Condition writeQ;
-    private int waitingReaders;
+	private int waitingReaders;
 	private int activeReaders;
 	private int waitingWriters;
 	private int activeWriters;
-    private int dbState;
+	private int dbState;
 
-    private int val;
+	private int val;
 
-    private static final int NOTUSED = 0;
-    private static final int READING = 1;
-    private static final int WRITING = 2;
+	private static final int NOTUSED = 0;
+	private static final int READING = 1;
+	private static final int WRITING = 2;
 
-    public DatabaseRW() {  
+	public DatabaseRW() {
 		m = new Mutex();
 		readQ = new Condition();
 		writeQ = new Condition();
 		activeReaders = waitingReaders = activeWriters = waitingWriters = 0;
 		dbState = NOTUSED;
 		val = 0;
-    }
+	}
 
 
-    public void StartRead() {
+	public void StartRead() {
 		m.Lock();
 		while (dbState == WRITING){
 			++waitingReaders;
@@ -41,50 +41,50 @@ public class DatabaseRW implements Database {
 		++activeReaders;
 		if (activeReaders == 1) dbState = READING;
 		m.UnLock();
-    }
+	}
 
 
-    public void EndRead() {
+	public void EndRead() {
 		m.Lock();
 		--activeReaders;
-		
+
 		if (activeReaders == 0 && waitingWriters > 0) { // I'm the last to read
-			dbState = NOTUSED;
-			writeQ.Signal();
-		}
-		m.UnLock();
-    }
-
-
-    public void StartWrite() {
-		m.Lock();
-		while (dbState != NOTUSED){
-			++waitingWriters;
-			writeQ.Wait(m);
-			--waitingWriters;
-		}	
-		++activeWriters;
-		dbState = WRITING;
-		m.UnLock();
-    }
-
-
-    public void EndWrite() {
-		m.Lock();
-		--activeWriters;
 		dbState = NOTUSED;
-		if(waitingWriters > 0) writeQ.Signal();
-		else readQ.Broadcast();
-		m.UnLock();
-    }
+		writeQ.Signal();
+	}
+	m.UnLock();
+}
 
 
-    public void Put( int i ) {
-		val = i;
-    }
+public void StartWrite() {
+	m.Lock();
+	while (dbState != NOTUSED){
+		++waitingWriters;
+		writeQ.Wait(m);
+		--waitingWriters;
+	}
+	++activeWriters;
+	dbState = WRITING;
+	m.UnLock();
+}
 
 
-    public int Get() {
-		return val;
-    }
+public void EndWrite() {
+	m.Lock();
+	--activeWriters;
+	dbState = NOTUSED;
+	if(waitingWriters > 0) writeQ.Signal();
+	else readQ.Broadcast();
+	m.UnLock();
+}
+
+
+public void Put( int i ) {
+	val = i;
+}
+
+
+public int Get() {
+	return val;
+}
 }
