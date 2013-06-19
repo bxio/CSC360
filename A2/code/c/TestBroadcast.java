cd27804c9da9adc7e75c44ff898495ba974dcb5e
Bill Xiong <billxiong99@gmail.com>
2013-06-19 14:01:26 -0700

    Fixed spacing.

--- A2/code/c/TestBroadcast.java
+++ A2/code/c/TestBroadcast.java
@@ -1,9 +1,9 @@
 /**
- This program creates a number of processes that all wait on a
- shared condition variable. The broadcast() method is used to
- wake up waiting processes. Processes waiting in the condition
- variable's queue must be run in the order of arrival.
- Expected output : "[a,[b,[c,[d,a],b],c],d]"
+This program creates a number of processes that all wait on a
+shared condition variable. The broadcast() method is used to
+wake up waiting processes. Processes waiting in the condition
+variable's queue must be run in the order of arrival.
+Expected output : "[a,[b,[c,[d,a],b],c],d]"
 */
 
 import java.util.*;
@@ -12,7 +12,7 @@ import uvic.posix.*;
 
 class TestBroadcast extends uvic.posix.Thread
 {
-    static Condition c = new Condition();
+	static Condition c = new Condition();
 	static Mutex	 m = new Mutex();
 
 	static TestBroadcast p1,p2,p3,p4;
@@ -22,40 +22,40 @@ class TestBroadcast extends uvic.posix.Thread
 		myname = name;
 	}
 
-    static void main(String[] args)
-    {
-        p1 = new TestBroadcast("a");
-        p2 = new TestBroadcast("b");
-        p3 = new TestBroadcast("c");
-        p4 = new TestBroadcast("d");
+	static void main(String[] args)
+	{
+		p1 = new TestBroadcast("a");
+		p2 = new TestBroadcast("b");
+		p3 = new TestBroadcast("c");
+		p4 = new TestBroadcast("d");
 
-	  	// kick start all threads
+  	// kick start all threads
 		p1.start(uvic.posix.Thread.SYSTEM);
 		p2.start(uvic.posix.Thread.SYSTEM);
 		p3.start(uvic.posix.Thread.SYSTEM);
 		p4.start(uvic.posix.Thread.SYSTEM);
-    }
+	}
 
 	public void run()
 	{
-	System.print("[" + myname);
-	 
-	if(myname == "a")
-	{
-	System.print(",");
-	yield();
-	    m.Lock();
-	    c.Broadcast();
-	    m.UnLock();
-	}
-	else
-	{
-	System.print(",");
-	m.Lock();
-	c.Wait(m);
-	    m.UnLock();
-	}
-	 
+		System.print("[" + myname);
+
+		if(myname == "a")
+		{
+			System.print(",");
+			yield();
+			m.Lock();
+			c.Broadcast();
+			m.UnLock();
+		}
+		else
+		{
+			System.print(",");
+			m.Lock();
+			c.Wait(m);
+			m.UnLock();
+		}
+
 	System.print(myname + "],");
 	}
 
