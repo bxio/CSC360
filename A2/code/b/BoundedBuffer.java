928b0f9429c039f4fb864aeeb641677b71a920ea
Bill Xiong <billxiong99@gmail.com>
2013-06-19 17:55:24 -0700

    Added comments for partB

--- A2/code/b/BoundedBuffer.java
+++ A2/code/b/BoundedBuffer.java
@@ -9,9 +9,7 @@ public class BoundedBuffer implements SBuffer{
 	private Condition nonfull, nonempty, waitingReaders;
 	private int buffer[];
 	private int size;
-	private int in, out, count, countReaders, rd, wr;
-	private boolean writerActive;
-	
+	private int in, out, count, countReaders;
 
 	public BoundedBuffer( int n ) {
 		m = new Mutex();
@@ -20,18 +18,15 @@ public class BoundedBuffer implements SBuffer{
 		waitingReaders = new Condition();
 		buffer = new int[n];
 		size = n;
-		count = 0;
-		countReaders = 0;
+		count = 0; /**< the number of things in the buffer **/
+		countReaders = 0; /**< Counts the number of active readers (readers currently reading content) */
 		in = 0;
 		out = 0;
-		rd = 0;
-		wr = 0;
-		writerActive = false;
 	}
 
 	/** Start reading an item at location i wait if buffer is empty. */
     public void StartRead( int i ) {
-		int j = i%size;
+		int j = i%size; /**< j is the virtual location, the "wrapped-around" location where you are trying to read */
 		m.Lock();
 		
 		if(j == out){ //if the thread is trying to read the same location as "out", continue
@@ -47,30 +42,25 @@ public class BoundedBuffer implements SBuffer{
     /** Done reading. */
     public void EndRead() {
 		if(countReaders == 0){
-
-			waitingReaders.Broadcast();
-			
+			waitingReaders.Broadcast(); //I'm last to read element. Everyone waiting for me should wake up and continue
 		}	
 	}
 
     /** Start writing an item wait if buffer is full. */
     public void StartWrite() {
-		
-		
-		
 			
 	}
 
     /** Done writing. */
     public void EndWrite() {
-		++wr;
+		
 	}
-	
 
 	public void Put( int i ) {
-		// DELETEME: Some starting code, but you might need to modify
 		m.Lock();
-		while (count == size) nonfull.Wait(m);
+		while (count == size) {
+			nonfull.Wait(m);
+		}
 		buffer[in] = i;
 		in = (in + 1) % size;
 		++count;
@@ -78,19 +68,19 @@ public class BoundedBuffer implements SBuffer{
 		m.UnLock();
 	}
 
-
 	public int Get() {
-		// DELETEME: Some starting code, but you might need to modify
 		int v;
 		m.Lock();
-		while (count == 0) nonempty.Wait(m);
+		while (count == 0){
+			nonempty.Wait(m);	
+		} 
 		v = buffer[out];
 		--countReaders;
-		if(countReaders == 0) {
+		if(countReaders == 0) { //We only increment out if you are the last reader. If you aren't, you have to wait for everyone else to catch up to you
 			out = (out + 1) % size;
 			--count;
 		}
-		nonfull.Signal();
+		nonfull.Signal(); //we removed an element, queue is no longer full
 		m.UnLock();
 
 		return v;
