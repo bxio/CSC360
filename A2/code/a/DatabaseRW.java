9d6813b1b9ef1baad0d1e43339b74a322718578b
Bill Xiong <billxiong99@gmail.com>
2013-06-19 18:18:49 -0700

    Moved test cases to its own folder, started Doxygen

--- A2/code/a/DatabaseRW.java
+++ A2/code/a/DatabaseRW.java
@@ -1,90 +1,92 @@
 import uvic.posix.*;
 
 /**
- This class defines a shared database for mulitple readers and
- writers to be implemented by you.
+This class defines a shared database for mulitple readers and
+writers to be implemented by you.
 */
 public class DatabaseRW implements Database {
 
 	private Mutex m;
-    private Condition readQ;
+	private Condition readQ;
 	private Condition writeQ;
-    private int waitingReaders;
-	private int activeReaders;
-	private int activeWriter;
-	private int waitingWriters;
-    private int dbState;
+	private int waitingReaders;/**< counts how many readers waiting **/
+	private int activeReaders; /**< counts how many readers are currently reading content **/
+	private boolean activeWriter;/**< Flag to see whether there is an active writer or not **/
+	private int waitingWriters;/**< counts how many writers are waiting to write **/
+	private int dbState;
 
-    private int val;
+	private int val;
+	//define for DBState
+	private static final int NOTUSED = 0;
+	private static final int READING = 1;
+	private static final int WRITING = 2;
 
-    private static final int NOTUSED = 0;
-    private static final int READING = 1;
-    private static final int WRITING = 2;
-
-    public DatabaseRW() {
+	public DatabaseRW() {
 		m = new Mutex();
 		readQ = new Condition();
 		writeQ = new Condition();
-		activeReaders = waitingReaders = activeWriter = waitingWriters = 0;
+		activeReaders = waitingReaders = waitingWriters = 0;
+		activeWriter = false;
 		dbState = NOTUSED;
 		val = 0;
-    }
+	}
 
 
-    public void StartRead() {
-	    	m.Lock();
+	public void StartRead() {
+		m.Lock();
 		if (dbState == WRITING){
 			++waitingReaders;
 			readQ.Wait(m);
 			--waitingReaders;
 		}
 		++activeReaders;
-		if (activeReaders == 1) dbState = READING;
-		m.UnLock();
-    }
-
-
-    public void EndRead() {
-		m.Lock();
-		--activeReaders;
-
-		if (activeReaders == 0 && waitingWriters > 0) { // I'm the last to read
-			dbState = WRITING;
-			writeQ.Signal();
+		if (activeReaders == 1) {
+			dbState = READING;
 		}
 		m.UnLock();
-    }
+	}
 
 
-    public void StartWrite() {
+	public void EndRead() {
 		m.Lock();
-		if (activeWriter == 1 && dbState != NOTUSED){
+		--activeReaders;
+	if (activeReaders == 0 && waitingWriters > 0) { // I'm the last to read
+		dbState = WRITING;//passes state directly to Writing
+		writeQ.Signal();//signal the writeQ
+	}
+		m.UnLock();
+	}
+
+
+	public void StartWrite() {
+		m.Lock();
+		if (activeWriter == true && dbState != NOTUSED){//If there is someone else writing AND state is not free, block me
 			++waitingWriters;
 			writeQ.Wait(m);
 			--waitingWriters;
 		}
-		activeWriter = 1;
+		activeWriter = true;
 		dbState = WRITING;
 		m.UnLock();
-    }
+	}
 
 
-    public void EndWrite() {
+	public void EndWrite() {
 		m.Lock();
-		activeWriter = 0;
+		activeWriter = false;
 		dbState = NOTUSED;
 		if(waitingWriters > 0) writeQ.Signal();
 		else readQ.Broadcast();
 		m.UnLock();
-    }
+	}
 
 
-    public void Put( int i ) {
+	public void Put( int i ) {
 		val = i;
-    }
+	}
 
 
-    public int Get() {
+	public int Get() {
 		return val;
-    }
+	}
 }
--- A2/code/mainpage.md
+++ A2/code/mainpage.md
@@ -10,14 +10,12 @@ Completed by:
 
 **A brief note about the Quantum**
 Please run all tests on Quantum 10. We've found that in certain cases, running Rose.exe on Quantum 1 fails.
-Put a brief overview of what was done in the assignment here. This file can be found and edited at code/mainpage.md.
-For more information on markdown, [see doxygen markdown documentation](http://www.stack.nl/~dimitri/doxygen/manual/markdown.html).
+Dr. Cheng has said that we don't need to fix this, as the size of the slice should not affect the correctness of the code.s
 
 To help organize your documents you can use headings like:
 
 ## Question 1 ##
 
-Discussion of Question 1. You can link and refer to classes and files just by typing their name, for example, Database or Database.java (source code).
 
 ## Question 2 ##
 
@@ -28,6 +26,3 @@ Discussion of Question 2. Talk about BoundedBuffer and TestP1C1.java.
 Discussion of Question 3.
 ##Test Cases##
 Run d/UnitTest.bin on quantum 10. The expected output is "[bb],[a[cc],a],"
-### Subheading ###
-
-You can create subheadings as well if you need that extra level of organization.
